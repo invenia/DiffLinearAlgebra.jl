7d0009f018088f3962757806feeece06710d545c
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2018-02-05 17:14:39 +0000

    Remove dependency on BenchmarkTools.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,14 @@ using DiffLinearAlgebra, Base.Test
 import FDM: assert_approx_equal, central_fdm
 import DiffLinearAlgebra: AA, AM, AVM, AS, ASVM, Arg1, Arg2
 
-import BenchmarkTools: @benchmark
-
 @testset "DiffLinearAlgebra" begin
     include("test_util.jl")
     include("util.jl")
-    # include("generic.jl")
-    # include("blas.jl")
-    # include("diagonal.jl")
-    # include("triangular.jl")
-    # include("uniformscaling.jl")
-    # include("factorization/cholesky.jl")
+    include("generic.jl")
+    include("blas.jl")
+    include("diagonal.jl")
+    include("triangular.jl")
+    include("uniformscaling.jl")
+    include("factorization/cholesky.jl")
     include("test_imports.jl")
 end
